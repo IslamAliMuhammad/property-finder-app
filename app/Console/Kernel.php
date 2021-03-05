5b65f59c6736ecc00bec183daf32f6592caec1b5
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2021-03-05 05:16:53 +0200

    Register migrate_in_order artisan command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use App\Console\Commands\MigrateInOrder;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -14,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        MigrateInOrder::class,
     ];
 
     /**

abdc12b6ff6e77af3b33c884c25558ecf74ffbec
Islam Ali Muhammad <islam.ali.muhammad@gmail.com>
2021-04-07 06:46:22 +0200

    Update MigrateInOrder command class

--- app/Console/Commands/MigrateInOrder.php
+++ app/Console/Commands/MigrateInOrder.php
@@ -52,13 +52,13 @@ class MigrateInOrder extends Command
             'database/migrations/2021_01_31_064317_create_amenities_table.php',
             'database/migrations/2021_02_01_042233_create_apartment_amenity_table.php',
             'database/migrations/2021_01_31_081126_create_apartment_photos_table.php',
-            'database\migrations\2021_03_05_192506_create_villa_types_table.php',
-            'database\migrations\2021_03_05_183720_create_villas_table.php',
-            'database\migrations\2021_03_05_191654_create_villa_photos_table.php',
-            'database\migrations\2021_03_05_192255_create_villa_amenity_table.php',
-            'database\migrations\2021_03_30_214634_create_land_types_table.php',
-            'database\migrations\2021_03_30_212335_create_lands_table.php',
-            'database\migrations\2021_03_30_213943_create_land_photos_table.php',
+            'database/migrations/2021_03_05_192506_create_villa_types_table.php',
+            'database/migrations/2021_03_05_183720_create_villas_table.php',
+            'database/migrations/2021_03_05_191654_create_villa_photos_table.php',
+            'database/migrations/2021_03_05_192255_create_villa_amenity_table.php',
+            'database/migrations/2021_03_30_214634_create_land_types_table.php',
+            'database/migrations/2021_03_30_212335_create_lands_table.php',
+            'database/migrations/2021_03_30_213943_create_land_photos_table.php',
             ];
 
             $rollbackMigrations = $this->option('reset');
@@ -76,3 +76,4 @@ class MigrateInOrder extends Command
             }
     }
 }
+
